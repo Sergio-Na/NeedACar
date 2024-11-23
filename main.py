6b3dc4083946c666b14a1f26757549dea2455093
Adam Simard <48142435+Simard302@users.noreply.github.com>
2024-11-23 15:28:47 -0500

    Modify chat to filter embed by sql query first

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+import textwrap
+from inputSql import generate_sql_from_input
+import pandas as pd
 
 # Ensure parallelism is disabled for tokenizers
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -25,6 +28,16 @@ chat = ChatGroq(
     groq_api_key=os.getenv("GROQ_API_KEY"),
     model_name="llama-3.2-90b-vision-preview"
 )
+
+# Initialize Groq chat model for SQL query gen
+sqlChat = ChatGroq(
+    temperature=0,
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="mixtral-8x7b-32768",
+)
+
+# Initialize the embedding model
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # Define system and human messages
 system = (
@@ -46,9 +59,16 @@ prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)]
 # Define the chain by combining the prompt with the chat model
 chain = prompt | chat
 
+# Load the data
+df = pd.read_csv('test.csv')
+conn = sqlite3.connect(':memory:')
+df.to_sql('cars', conn, index=False, if_exists='replace')
+
 # Conversational loop
 def chat_with_user():
     print("Hello! Ask me anything about the vehicles database or type 'quit' to exit.")
+
+    # ex: "how me a car that is before 2015, less than 30'000$ and automatic transmission"
     while True:
         user_input = input("\nYou: ").strip()
         if user_input.lower() in ["quit", "exit"]:
@@ -56,6 +76,13 @@ def chat_with_user():
             break
 
         try:
+            sql_query = generate_sql_from_input(user_input, sqlChat)
+
+            filtered_df = pd.read_sql_query(sql_query.content, conn)
+            texts = filtered_df["description"].tolist()
+            metadata = filtered_df.drop(columns=["description"]).to_dict(orient="records")
+            vectorstore = FAISS.from_texts(texts, embeddings, metadatas=metadata)
+
             # Perform similarity search (limit results to k=3)
             search_results = vectorstore.similarity_search(user_input, k=3)
 
@@ -69,6 +96,7 @@ def chat_with_user():
             response = chain.invoke({"query": user_input, "results": formatted_results})
             print(f"\nAssistant: {response.content}")
         except Exception as e:
+            raise e
             print(f"Error: {e}")
 
 if __name__ == "__main__":
